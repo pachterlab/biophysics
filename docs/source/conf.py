afe1fbf7480c9d124b38261354d4f7e01050549a
tarachari3 <tarachari3@gmail.com>
2024-02-25 12:05:30 -0800

    Removing Lumache

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Pachter Biophysics'
+copyright = '2024, Pachter Lab'
+author = 'Pachter Lab Members'
 
 release = '0.1'
 version = '0.1.0'
