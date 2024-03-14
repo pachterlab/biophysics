6fab81dff22689dd5a431743b481c066f453bc18
CatherineFelce <109311417+CatherineFelce@users.noreply.github.com>
2024-03-14 09:55:22 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx_panels',
+    'sphinx.ext.sphinx_panels',
 ]
 
 intersphinx_mapping = {
