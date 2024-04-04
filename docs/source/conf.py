ac886501205a0811f7ed4ea936bf15528a4e222a
Meichen Fang <75282508+meichenfang@users.noreply.github.com>
2024-04-04 12:49:56 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,8 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx'
+    'sphinx.ext.intersphinx',
+    'sphinx_panels'
 ]
 
 intersphinx_mapping = {
