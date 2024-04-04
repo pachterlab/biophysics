58f79da0026009785a7ec387f4cd0e1983ab9df6
Meichen Fang <75282508+meichenfang@users.noreply.github.com>
2024-04-04 12:46:01 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,13 +10,13 @@ release = '0.1'
 version = '0.1.0'
 
 # -- General configuration
-
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    "sphinx_design"
 ]
 
 intersphinx_mapping = {
